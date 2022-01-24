627670c3f072d5483553bbc44efb3b0606b4d7df
Abdulrazaq Alagbada <razaqalagbada@gmail.com>
2022-01-24 11:11:19 -0600

    front-end and backend setup

---
+++ warehouse-frontend/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+        <h3>Hello from react front end app page</h3>
+    </div>
+  );
+}
+
+export default App;
