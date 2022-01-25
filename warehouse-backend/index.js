bbc593598ffd2dd487fab744fb25bb6efa7e52dd
Abdulrazaq Alagbada <razaqalagbada@gmail.com>
2022-01-25 11:23:27 -0600

    user seed file and setup

--- warehouse-backend/index.js
+++ warehouse-backend/index.js
@@ -11,11 +11,16 @@ async function seed(){
     await db.sync({force: true});
 
     const seedPath = path.join(__dirname,'item.json')
+    const seedPath2 = path.join(__dirname,'user.json')
     const buffer = await fs.readFile(seedPath)
+    const buffer2 = await fs.readFile(seedPath2)
     const {data} = JSON.parse(String(buffer))
+    const {userdata} = JSON.parse(String(buffer2))
     const itemPromises = data.map(item =>  Item.create(item))
+    const itemPromises2 = userdata.map(item =>  User.create(item))
     
     await Promise.all(itemPromises)
+    await Promise.all(itemPromises2)
     console.log("Items seeded")
 }
 
