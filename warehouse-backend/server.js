12338b24248e83b651cce6df53ef8270ebb86790
Abdulrazaq Alagbada <razaqalagbada@gmail.com>
2022-01-25 12:26:31 -0600

    signup and login setup

--- warehouse-backend/server.js
+++ warehouse-backend/server.js
@@ -12,7 +12,7 @@ const cors = require('cors')
 
 app.use(cors())
 
-
+app.use(express.json());
 app.get('/items', async (req, res) => {
     const items = await Item.findAll();
     res.json({items})
@@ -31,6 +31,42 @@ app.get('/items/:id', async (req, res) => {
 app.get('/users/:id', async (req, res) => {
     const user = await User.findByPk(req.params.id)
     res.json({user})
+})
+
+app.post('/signup', async (req,res) =>{
+    try {
+
+    const newuser = await User.create(req.body)
+    res.send({message:"User successfully created", user: newuser})
+
+    } catch (error) {
+      res.json({error})  
+    }
+    
+})
+
+app.post('/user/login', async (req,res) =>{
+    try {
+
+        console.log(req)
+        const user = await User.findOne({
+            where:{
+                email : req.body.email
+            }
+        })
+        console.log(user)
+        if(user.password === req.body.password){
+            res.json({message: "Login successful", user: user})
+        }else{
+            res.status(401)
+            res.json({error:"email or password is incorrect"})
+        }
+        
+    } catch (error) {
+        console.log(error)
+        res.status(400)
+       res.json({error: "email or password is incorrect"}) 
+    }
 })
 
 app.listen(port, async() => {
