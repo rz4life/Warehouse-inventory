b3a6e63cbc9ca0119a5349f6d639b1b43e72c5b7
Shafee Rushdan <srushdan@mbp-010668.myfiosgateway.com>
2022-01-27 12:03:32 -0800

    Home page & other bug fixes

--- warehouse-frontend/src/components/Home.js
+++ warehouse-frontend/src/components/Home.js
@@ -1,16 +1,24 @@
-
+import ".//Home.css";
 const Home = () => {
   return (
-    <div >
-      <p className="home-paragraph">
-        {" "}
-        Welcome to Wind Warehouse
-        <br></br>
-        <br></br>
-        An interactive react application created by Shafee, RZ and Andrés
-        <br></br>
-        <br></br> Please signup or sign in to begin
-      </p>
+    <div>
+      <div class="backgroundpicIV">
+        <div class="home">
+
+          <div class="home-card">
+
+            <p className="home-paragraph">
+              {" "}
+              Welcome to Wind Warehouse
+              <br></br>
+              <br></br>
+              An interactive react application created by Shafee, RZ and Andrés
+              <br></br>
+              <br></br> Please signup or sign in to begin
+            </p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
--- warehouse-frontend/src/containers/NewItemForm.js
+++ warehouse-frontend/src/containers/NewItemForm.js
@@ -13,7 +13,7 @@ class NewItemForm extends Component {
       price: "",
       category: "",
       quantity: "",
-    image: "",
+      image: "",
     };
   }
 
@@ -22,7 +22,7 @@ class NewItemForm extends Component {
 
 
     this.props.addNewItem(this.state);
-    
+
   };
   onChangeHandler = (e) => {
     this.setState({ [e.target.name]: e.target.value });
@@ -30,67 +30,74 @@ class NewItemForm extends Component {
     console.log([e.target.name], ":", e.target.value);
   };
 
- 
+
 
   render() {
     return (
       <>
-        <div className="centered">
-          <form onSubmit={this.submitHandler}>
-            <h4>ADD MEDICATION</h4>
-            <input
-              type="text"
-              placeholder="Name"
-              value={this.state.name}
-              onChange={this.onChangeHandler}
-              name="name"
-            />{" "}
-            <br></br>
-            <input
-              type="text"
-              placeholder="Description"
-              value={this.state.description}
-              onChange={this.onChangeHandler}
-              name="description"
-            />{" "}
-            <br></br>
-            <input
-              type="integer"
-              placeholder="Price"
-              value={this.state.price}
-              onChange={this.onChangeHandler}
-              name="price"
-            />
-            <br></br>
-            <input
-              type="text"
-              placeholder="Category"
-              value={this.state.category}
-              onChange={this.onChangeHandler}
-              name="category"
-            />{" "}
-            <br></br>
-            <input
-              type="integer"
-              placeholder="Stock"
-              value={this.state.quantity}
-              onChange={this.onChangeHandler}
-              name="quantity"
-            />
-            <br></br>
-            <input
-            type="text"
-            placeholder="Image"
-            value={this.state.image}
-            onChange={this.onChangeHandler}
-            name="image"
-          />{" "}
-            <br></br>
-            <br></br>
-            <input type="submit" value="submit" />
-          </form>
+        <div class="new">
+          <div class="backgroundpicnew">
+            <div class="item-card-new">
 
-          <br></br>
+            <div className="centered">
+              <form onSubmit={this.submitHandler}>
+                <h4>ADD ITEM</h4>
+                <input
+                  type="text"
+                  placeholder="Name"
+                  value={this.state.name}
+                  onChange={this.onChangeHandler}
+                  name="name"
+                  />{" "}
+                <br></br>
+                <input
+                  type="text"
+                  placeholder="Description"
+                  value={this.state.description}
+                  onChange={this.onChangeHandler}
+                  name="description"
+                  />{" "}
+                <br></br>
+                <input
+                  type="integer"
+                  placeholder="Price"
+                  value={this.state.price}
+                  onChange={this.onChangeHandler}
+                  name="price"
+                  />
+                <br></br>
+                <input
+                  type="text"
+                  placeholder="Category"
+                  value={this.state.category}
+                  onChange={this.onChangeHandler}
+                  name="category"
+                  />{" "}
+                <br></br>
+                <input
+                  type="integer"
+                  placeholder="Stock"
+                  value={this.state.quantity}
+                  onChange={this.onChangeHandler}
+                  name="quantity"
+                  />
+                <br></br>
+                <input
+                  type="text"
+                  placeholder="Image"
+                  value={this.state.image}
+                  onChange={this.onChangeHandler}
+                  name="image"
+                  />{" "}
+                <br></br>
+                <br></br>
+                <button type= "submit" value="submit" >Submit</button>
+              </form>
+
+              <br></br>
+                  </div>
+            </div>
+          </div>
         </div>
       </>
     );
