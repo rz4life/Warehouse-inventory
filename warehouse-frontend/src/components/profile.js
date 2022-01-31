c9429c747f648a10cf96b7732e0317d371786567
Shafee Rushdan <srushdan@mbp-010668.myfiosgateway.com>
2022-01-31 08:16:42 -0800

    cssprofile

--- warehouse-frontend/src/components/profile.js
+++ warehouse-frontend/src/components/profile.js
@@ -1,15 +1,28 @@
-const Profile =  (props) =>{
+import "./profile.css"
+const Profile = (props) => {
 
 
-    return(
+    return (
+
         <div>
-            <h1> Profile Page</h1>
-            <h3><img className ="avatar" height="20%" width="20%" src= {props.user.data.user.profilepicture}alt="User Profile" /></h3>
-            <h3>{props.user.data.user.firstName}</h3>
-            <h3>{props.user.data.user.lastName}</h3>
-            <h3>{props.user.data.user.email}</h3>
+            <div class="profile">
+                <div class="backgroundpicV">
+                    <div class="profile-card">
+                        <h1 class="profile-grey"> Profile Page</h1>
+                        <img className="avatar" height="20%" width="20%" src={props.user.data.user.profilepicture} alt="User Profile" />
+                        <h3 class="profile-grey">{props.user.data.user.firstName}</h3>
+                        <h3>{props.user.data.user.lastName}</h3>
+                        <h3>{props.user.data.user.email}</h3>
+                    </div>
+                </div>
+
+            </div>
+
 
         </div>
+
+
+
     )
 
 }
